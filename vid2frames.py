630bd61cabb36d23631af5635852c62cef120d0d
ul23utyn <belen.lojo@fau.de>
2023-07-06 13:58:52 +0200

    split video in frames. already sharpened.

---
+++ vid2frames.py
@@ -0,0 +1,29 @@
+import cv2
+import os
+
+def vid2frames(video,fps=3):
+        
+    vidcap = cv2.VideoCapture('video')
+    success, image = vidcap.read()
+    count = 0
+    os.mkdir('./frames')
+    os.chdir('./frames')
+
+    desired_fps = 2
+
+    frame_rate = vidcap.get(cv2.CAP_PROP_FPS)
+    frame_delay = int(frame_rate / desired_fps)
+    frame_counter = 0
+
+    while success:
+        if frame_counter % frame_delay == 0:
+            # Apply unsharp mask filter
+            blurred = cv2.GaussianBlur(image, (0, 0), 2)
+            sharpened_image = cv2.addWeighted(image, 1.5, blurred, -0.5, 0)
+
+            cv2.imwrite("frame%d.jpg" % count, sharpened_image)
+            print('Read a new frame:', success)
+            count += 1
+
+        success, image = vidcap.read()
+        frame_counter += 1
