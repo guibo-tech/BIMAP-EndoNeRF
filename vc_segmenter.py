7917befa5570a45844db0fce183ae288b658175e
Guilherme <med.gborges@gmail.com>
2023-07-21 22:10:42 +0200

    [IMPROVE] Update vocal cord segmenter - update the code to run the Python file as a command and pass the image directory - update code to open segment window for the user - update the code to show 6 methods for segmentation, for tests

--- vc_segmenter.py
+++ vc_segmenter.py
@@ -9,8 +9,13 @@
 # is drawn on the mask using the filtered contours. Finally, the mask is applied to the original image
 # using a bitwise operation to segment the vocal cord hole. The segmented vocal cord hole is displayed using cv2.imshow.
 
-import cv2
 import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+import argparse
+
+# Example Usage
+# python vc_segmenter.py --directory /data/images/frame40.png
 
 def select_roi(image):
     # Display the image and allow the user to draw a ROI
@@ -69,198 +74,83 @@ def segment_vocal_cord_hole(image_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+# It shows 6 different methods for tests
+# Image operation using thresholding
+def segment_vocal_methods(image_path):
+    # Read the image file
+    with open(image_path, 'rb') as f:
+        image_data = f.read()
+    img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
 
-# Path to the endoscopy image
-image_path = 'data_source/reco_001/segmented_png/vocal_6.png'
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-# Perform vocal cord hole segmentation
-segment_vocal_cord_hole(image_path)
+    # # Image operation using thresholding
+    # image_path1 = 'data_source/reco_001/segmented_png/vocal_18 - 2.jpg'
+    # image_path2 = 'data_source/reco_001/segmented_png/coin-detection.jpg'
+    # img = cv2.imread(image_path1)
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # newimg - bitwise_not
+    darker = cv2.equalizeHist(gray)
+    ret,thresh = cv2.threshold(darker,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    newimg = cv2.bitwise_not(thresh)
 
-#########################################################
-# THIS IS WORKING DISPLAYS 6 plots
+    # Change the threshold value here
+    threshold_value = 100
 
-# import numpy as np
-# import cv2
-# from matplotlib import pyplot as plt
-#
-# # Image operation using thresholding
-# def segment_vocal_cord_hole(image_path):
-#     # Read the image file
-#     with open(image_path, 'rb') as f:
-#         image_data = f.read()
-#     img = cv2.imdecode(np.frombuffer(image_data, np.uint8), cv2.IMREAD_COLOR)
-#
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-#     # # Image operation using thresholding
-#     # image_path1 = 'data_source/reco_001/segmented_png/vocal_18 - 2.jpg'
-#     # image_path2 = 'data_source/reco_001/segmented_png/coin-detection.jpg'
-#     # img = cv2.imread(image_path1)
-#
-#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-#     # newimg - bitwise_not
-#     darker = cv2.equalizeHist(gray)
-#     ret,thresh = cv2.threshold(darker,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-#     newimg = cv2.bitwise_not(thresh)
-#
-#     # Change the threshold value here
-#     threshold_value = 100
-#
-#     # Simple Thresholding
-#     _, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
-#
-#     # Adaptive Thresholding
-#     adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
-#
-#     # Otsu's Thresholding
-#     _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-#
-#     # Close area
-#     # Noise removal using Morphological
-#     # closing operation
-#     kernel = np.ones((3, 3), np.uint8)
-#     closing = cv2.morphologyEx(binary, cv2.MORPH_CLOSE,
-#                                kernel, iterations=2)
-#
-#     # Background area using Dilation
-#     bg = cv2.dilate(closing, kernel, iterations=1)
-#
-#     # Finding foreground area
-#     dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
-#     ret, fg = cv2.threshold(dist_transform, 0.02
-#                             * dist_transform.max(), 255, 0)
-#
-#
-#     # Display the results
-#     titles = ['Original', 'Simple Thresholding', 'Adaptive Thresholding', "Otsu's Thresholding", 'newimg', 'fg']
-#     images = [gray, binary, adaptive, otsu, newimg, fg]
-#
-#     for i in range(6):
-#         plt.subplot(2, 3, i + 1)
-#         plt.imshow(images[i], 'gray')
-#         plt.title(titles[i])
-#         plt.axis('off')
-#
-#     plt.show()
-#
-# # Path to the endoscopy image
-# image_path = 'data_source/reco_001/segmented_png/vocal_18_roi.png'
-#
-# # Perform vocal cord hole segmentation
-# segment_vocal_cord_hole(image_path)
+    # Simple Thresholding
+    _, binary = cv2.threshold(gray, threshold_value, 255, cv2.THRESH_BINARY)
+
+    # Adaptive Thresholding
+    adaptive = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    # Otsu's Thresholding
+    _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+    # Close area
+    # Noise removal using Morphological
+    # closing operation
+    kernel = np.ones((3, 3), np.uint8)
+    closing = cv2.morphologyEx(binary, cv2.MORPH_CLOSE,
+                               kernel, iterations=2)
+
+    # Background area using Dilation
+    bg = cv2.dilate(closing, kernel, iterations=1)
+
+    # Finding foreground area
+    dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
+    ret, fg = cv2.threshold(dist_transform, 0.02
+                            * dist_transform.max(), 255, 0)
 
 
+    # Display the results
+    titles = ['Original', 'Simple Thresholding', 'Adaptive Thresholding', "Otsu's Thresholding", 'newimg', 'fg']
+    images = [gray, binary, adaptive, otsu, newimg, fg]
 
-###########################################################
+    for i in range(6):
+        plt.subplot(2, 3, i + 1)
+        plt.imshow(images[i], 'gray')
+        plt.title(titles[i])
+        plt.axis('off')
 
-#
-# import numpy as np
-# import cv2
-# from matplotlib import pyplot as plt
-#
-# # Image operation using thresholding
-# image_path1 = 'data_source/reco_001/segmented_png/vocal_18 - 2.jpg'
-# image_path2 = 'data_source/reco_001/segmented_png/coin-detection.jpg'
-# img = cv2.imread(image_path2)
-#
-#
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#
-# # Change the threshold value here (e.g., 150)
-# threshold_value = 200
-#
-# ret, thresh = cv2.threshold(gray, threshold_value, 255,
-#                             cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-#
-# # Noise removal using Morphological
-# # closing operation
-# kernel = np.ones((3, 3), np.uint8)
-# closing = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE,
-#                            kernel, iterations=2)
-#
-# # Background area using Dilation
-# bg = cv2.dilate(closing, kernel, iterations=1)
-#
-# # Finding foreground area
-# dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 0)
-# ret, fg = cv2.threshold(dist_transform, 0.02
-#                         * dist_transform.max(), 255, 0)
-#
-# cv2.imshow('image', fg)
-# # cv2.imshow('image', thresh)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    plt.show()
 
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser(description='Vocal Cord Hole Segmentation')
 
+    # Add an argument for the image directory
+    parser.add_argument('--directory', type=str, required=True, help='Path to the endoscopy image directory')
 
-###########################################################
+    # Parse the command-line arguments
+    args = parser.parse_args()
 
-# Perform vocal cord hole segmentation
-# segment_vocal_cord_hole(image_path)
+    # Get the image directory path from the command-line argument
+    image_dir = args.directory
 
-#
-# import cv2
-# import numpy as np
-# from skimage import measure
-#
-#
-# def segment_vocal_cord_hole(image_path):
-#     # Load the endoscopy image
-#     image = cv2.imread(image_path, 0)  # Load as grayscale
-#
-#     # Preprocess the image
-#     blurred = cv2.GaussianBlur(image, (5, 5), 0)
-#     threshold = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-#
-#     # Perform morphological operations to enhance the vocal cord hole
-#     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
-#     opening = cv2.morphologyEx(threshold, cv2.MORPH_OPEN, kernel, iterations=2)
-#     sure_bg = cv2.dilate(opening, kernel, iterations=3)
-#
-#     # Identify sure foreground area
-#     dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
-#     _, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
-#
-#     # Find the unknown region
-#     sure_fg = np.uint8(sure_fg)
-#     unknown = cv2.subtract(sure_bg, sure_fg)
-#
-#     # Marker labelling
-#     _, markers = cv2.connectedComponents(sure_fg)
-#
-#     # Add one to all labels so that sure background is not 0 but 1
-#     markers = markers + 1
-#
-#     # Mark the region of unknown with zero
-#     markers[unknown == 255] = 0
-#
-#     # Apply watershed algorithm for final segmentation
-#     markers = cv2.watershed(image, markers)
-#     labeled_image = np.zeros(image.shape, dtype=np.uint8)
-#     labeled_image[markers > 1] = 255
-#
-#     # Find contours of the segmented vocal cord hole
-#     contours, _ = cv2.findContours(labeled_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#
-#     # Filter contours based on area
-#     min_area = 100  # Minimum contour area threshold
-#     filtered_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_area]
-#
-#     # Draw the contours on the original image
-#     result = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
-#     cv2.drawContours(result, filtered_contours, -1, (0, 255, 0), 2)
-#
-#     # Display the segmented vocal cord hole
-#     cv2.imshow("Segmented Vocal Cord Hole", result)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
-#
-#
-# # Path to the endoscopy image
-# image_path = "data_source/reco_001/segmented_png/vocal_18.png"
-#
-# # Perform vocal cord hole segmentation
-# segment_vocal_cord_hole(image_path)
+    # Perform vocal cord hole segmentation, show different methods
+    segment_vocal_methods(image_dir)
+
+    # Perform vocal cord hole segmentation
+    segment_vocal_cord_hole(image_dir)
